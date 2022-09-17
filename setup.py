2eac34e2a0d095204dda79aa462a9d664b77efa5
ingHFranco <inghfrancoo@gmail.com>
2022-09-17 09:46:52 -0500

    Creation of the project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='first work in the course "Data science in production" for the master's degree in engineering with emphasis in data analytics, at the Universidad del Quindio.',
+    author='Humberto Franco - Felipe Padilla',
+    license='MIT',
+)
