88817ded1b680c72970aaabfd3925ee0b91265e6
ingHFrancoO <inghfrancoo@gmail.com>
2022-09-24 10:40:58 -0500

    Correction description

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='first work in the course "Data science in production" for the master's degree in engineering with emphasis in data analytics, at the Universidad del Quindio.',
+    description='first work in the course "Data science in production" for the masters degree in engineering with emphasis in data analytics, at the Universidad del Quindio.',
     author='Humberto Franco - Felipe Padilla',
     license='MIT',
 )
